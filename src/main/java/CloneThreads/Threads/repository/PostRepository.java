6f4c3c578c1aede4218c5eb3c27a50c20be0aea2
NT-THTRM\nthuy <nthuytram2815@gmail.com>
2025-11-17 19:29:01 +0700

    fix PostRepository

--- src/main/java/CloneThreads/Threads/repository/PostRepository.java
+++ src/main/java/CloneThreads/Threads/repository/PostRepository.java
@@ -16,11 +16,14 @@ public interface PostRepository extends JpaRepository<Post, String> {
 //	    )
 //    List<Post> searchPosts(@Param("keyword") String keyword);
 	@Query(
-		    value = "SELECT p.*, u.full_name as authorName, u.username as authorUsername " +
-		            "FROM posts p " +
-		            "JOIN users u ON p.user_id = u.id " +
-		            "WHERE p.content ILIKE CONCAT('%', :keyword, '%')",
-		    nativeQuery = true
-		)
+			value = "SELECT p.id, p.user_id AS \"userId\", p.content, " +
+					"p.media_type AS \"mediaType\", p.media_url AS \"mediaUrl\", p.media_public_id AS \"mediaPublicId\", " +
+					"p.scope, p.created_at AS \"createdAt\", p.updated_at AS \"updatedAt\", " +
+					"u.full_name AS \"fullName\", u.user_name AS \"username\", u.avatar_url AS \"avatarUrl\" " +
+					"FROM posts p " +
+					"JOIN users u ON p.user_id = u.id " +
+					"WHERE p.content ILIKE CONCAT('%', :keyword, '%')",
+			nativeQuery = true
+	)
 	List<Map<String, Object>> searchPosts(@Param("keyword") String keyword);
 }
